378f89a38201782494a82b87953e48891489d3ee
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-10-19 00:58:47 +0700

    menyiapkan MainActivity dengan nama QuestBasic

---
+++ app/src/main/java/com/example/questbasic/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.questbasic
+
+import android.graphics.drawable.shapes.OvalShape
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questbasic.ui.theme.QuestBasicTheme
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import com.example.questbasic.ui.theme.QuestBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QuestBasicTheme {
+
+}
